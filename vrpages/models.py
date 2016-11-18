d9b275708e3fdb25f517811d5894c562cd555a09
Renelle <titingrenelle@gmail.com>
2016-11-18 16:20:05 +0800

    create vrpage model

--- vrpages/models.py
+++ vrpages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class VRPage(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
+    client = models.CharField(max_length=255)
+    url = models.URLField(max_length=200)
