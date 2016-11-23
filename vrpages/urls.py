5fa52e563d2f7f6927a6a67c8e97de33d1b88fe7
Renelle <titingrenelle@gmail.com>
2016-11-24 06:51:35 +0800

    create polish list page and qualify list page

--- vrpages/urls.py
+++ vrpages/urls.py
@@ -3,9 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.vrpage_list, name='list'),
-    url(r'(?P<vrpage_pk>\d+)/(?P<rawurl_pk>\d+)/$', views.rawurl_detail,
-        name='rawurl'),
-    url(r'(?P<pk>\d+)/qualify/$', views.rawurl_qualify, name='rawurl_qualify'),
-    url(r'(?P<pk>\d+)/polish/$', views.rawurl_polish, name='rawurl_polish'),
+    url(r'^qualify/$', views.vrpage_qualify_list, name='qualify_list'),
+    url(r'^polish/$', views.vrpage_polish_list, name='polish_list'),
+    url(r'^(?P<pk>\d+)/qualify/$', views.rawurl_qualify, name='rawurl_qualify'),
+    url(r'^(?P<pk>\d+)/polish/$', views.rawurl_polish, name='rawurl_polish'),
 ]
--- vrpages/views.py
+++ vrpages/views.py
@@ -6,26 +6,21 @@ from . import forms
 from . import models
 
 
-def vrpage_list(request):
+def vrpage_qualify_list(request):
     vrpages = models.VRPage.objects.all()
-    return render(request, 'vrpages/vrpage_list.html', {'vrpages': vrpages})
+    return render(request, 'vrpages/vrpage_qualify_list.html', {'vrpages': vrpages})
 
 
-def vrpage_detail(request, pk):
-    vrpage = get_object_or_404(models.VRPage, pk=pk)
-    return render(request, 'vrpages/vrpage_detail.html', {'vrpage': vrpage})
-
-
-def rawurl_detail(request, vrpage_pk, rawurl_pk):
-    raw_url = get_object_or_404(models.RawUrl, vrpage_id=vrpage_pk, id=rawurl_pk)
-    return render(request, 'vrpages/rawurl_detail.html', {'raw_url': raw_url})
+def vrpage_polish_list(request):
+    vrpages = models.VRPage.objects.all()
+    return render(request, 'vrpages/vrpage_polish_list.html', {'vrpages': vrpages})
 
 
 def rawurl_qualify(request, pk):
     try:
         rawurl = models.RawUrl.objects.filter(vrpage_id=pk).filter(checked=False)[0]
     except IndexError:
-        return HttpResponseRedirect(reverse('vrpages:list'))
+        return HttpResponseRedirect(reverse('vrpages:qualify_list'))
     else:
         form = forms.RawUrlForm(instance=rawurl)
         if request.method == 'POST':
@@ -43,7 +38,7 @@ def rawurl_polish(request, pk):
             qualified=True).filter(
             polishurl__email__isnull=True)[0]
     except IndexError:
-        return HttpResponseRedirect(reverse('vrpages:list'))
+        return HttpResponseRedirect(reverse('vrpages:polish_list'))
     else:
         form = forms.PolishUrlForm(initial={'polished_url': rawurl.url, 'rawurl': rawurl.pk})
         if request.method == 'POST':
