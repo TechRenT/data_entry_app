58cc63248e5cd7f7a9d07e46f9fba7636d5c89d4
Renelle <titingrenelle@gmail.com>
2016-12-01 12:47:12 +0800

    Register and set-up PolishURL, Bounce, Unsubscribe models in admin.py to allow import/export of data

--- vrpages/admin.py
+++ vrpages/admin.py
@@ -17,21 +17,76 @@ class RawUrlResource(resources.ModelResource):
 
     class Meta:
         model = models.RawUrl
-        fields = ('id', 'domain', 'url', 'source', 'batch', 'checked', 'qualified', 'vrpage')
+        fields = (
+            'id',
+            'domain',
+            'url',
+            'source',
+            'batch',
+            'checked',
+            'qualified',
+            'vrpage'
+        )
 
     def skip_row(self, instance, original):
         rawurls = models.RawUrl.objects.filter(vrpage=instance.vrpage)
         for rawurl in rawurls:
-            if rawurl.domain == instance.domain:
+            if rawurl.url == instance.url:
                 return True
         else:
             return False
+
+
+class UnsubscribeResource(resources.ModelResource):
+
+    class Meta:
+        model = models.Unsubscribe
+        fields = ('id', 'email', 'vrpage')
+
+
+class BounceResource(resources.ModelResource):
+
+    class Meta:
+        model = models.Bounce
+        fields = ('id', 'email', 'vrpage')
+
+
+class PolishUrlResource(resources.ModelResource):
+
+    class Meta:
+        model = models.PolishUrl
+        fields = (
+            'id',
+            'created_at',
+            'rawurl',
+            'polished_url',
+            'email',
+            'page_title',
+            'contact_name',
+            'broken_link',
+            'domain_authority',
+        )
+
 
 @admin.register(models.RawUrl)
 class RawUrlAdmin(ImportExportModelAdmin):
     resource_class = RawUrlResource
 
 
+@admin.register(models.Unsubscribe)
+class UnsubscribeAdmin(ImportExportModelAdmin):
+    resource_class = UnsubscribeResource
+
+
+@admin.register(models.Bounce)
+class BounceAdmin(ImportExportModelAdmin):
+    resource_class = BounceResource
+
+
+@admin.register(models.PolishUrl)
+class PolishUrlAdmin(ImportExportModelAdmin):
+    resource_class = PolishUrlResource
+
+
 admin.site.register(models.VRPage, VRPageAdmin)
-admin.site.register(models.PolishUrl)
 admin.site.register(models.AssignRawUrl)
